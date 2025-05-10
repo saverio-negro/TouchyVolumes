faa1808f3194f5d8a0aa64a997b5c72a460e02c2
Saverio Negro <saverionegro@gmail.com>
2025-05-10 23:36:26 +0200

    Initial Commit

---
+++ TouchyVolumes/TouchyVolumesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchyVolumesApp.swift
+//  TouchyVolumes
+//
+//  Created by Saverio Negro on 10/05/25.
+//
+
+import SwiftUI
+
+@main
+struct TouchyVolumesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
